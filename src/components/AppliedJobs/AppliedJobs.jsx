0b5bf278cefa4a0c8961e1e6c9a85e8dd5d7a94b
Naim Ali <naimalinaim1@gmail.com>
2023-04-10 10:41:31 +0600

    Filter by remote and on-site functionality added

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -6,6 +6,7 @@ import ViewAppliedJob from "../ViewAppliedJob/ViewAppliedJob";
 const AppliedJobs = () => {
   const [allJobs, setAllJobs] = useState([]);
   const [appliedJobs, setAppliedJobs] = useState([]);
+  const [searchJob, setSearchJob] = useState([]);
 
   //   load all jobs
   useEffect(() => {
@@ -25,14 +26,42 @@ const AppliedJobs = () => {
     setAppliedJobs(jobs);
   }, [allJobs]);
 
+  // filter by select menu
+  const handleFilterJob = (e) => {
+    const value = e.target.value;
+    const filterText = value == 1 ? "remote" : value == 2 ? "onsite" : "";
+
+    const getFilterJob =
+      filterText.length > 0 &&
+      appliedJobs.filter(
+        (job) => job.remote_or_onsite.toLowerCase() === filterText
+      );
+
+    setSearchJob(getFilterJob);
+  };
+
   return (
     <main>
       <OtherBanner title="Applied Jobs" />
-      <div className="px-10 sm:px-4 md:px-8 lg:px-24 mt-36 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 gap-4">
-        {appliedJobs[0]?.id &&
-          appliedJobs.map((item) => (
-            <ViewAppliedJob key={item?.id} item={item} />
-          ))}
+      <div className="mt-36 mb-10 text-right px-10 sm:px-4 md:px-8 lg:px-24">
+        <select
+          onChange={handleFilterJob}
+          className="bg-gray-300 p-2 px-5 rounded-md cursor-pointer"
+        >
+          <option>Filter All</option>
+          <option value={1}>Remote Job</option>
+          <option value={2}>On-site Job</option>
+        </select>
+      </div>
+      <div className="px-10 sm:px-4 md:px-8 lg:px-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 gap-4">
+        {searchJob[0]?.id
+          ? searchJob.map((item) => (
+              <ViewAppliedJob key={item?.id} item={item} />
+            ))
+          : appliedJobs[0]?.id &&
+            appliedJobs.map((item) => (
+              <ViewAppliedJob key={item?.id} item={item} />
+            ))}
       </div>
     </main>
   );
